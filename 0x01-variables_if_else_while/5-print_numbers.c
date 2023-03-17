dc34e12d70cbcf4015a7bc36a28279d483baa2e0
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-03-17 17:47:05 +0200

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Discribtion: 'numbers from 0-9'
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i == 0 ; i <= 9 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}

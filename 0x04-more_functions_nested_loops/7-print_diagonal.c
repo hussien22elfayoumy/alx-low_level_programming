68a09fcf221a9b0cc3b429b6e3d391e057354b7c
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-03-23 22:46:13 +0200

    mmm

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,26 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int x, y;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (x = 1; x <= n; x++)
 		{
-			for (j = 1; j <= n; j++)
+			for (y = 1; y <= n; y++)
 			{
-				if (j == i)
+				if (x == y)
+				{
 					_putchar(92);
-				else
-					_putchar(' ');
+					break;
+				}
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }

2173db923fd8af2957053c1728d180919bd211e5
hussien22elfayoumy <hussienmedo36@gmail.com>
2023-03-28 03:13:42 +0200

    reverse 2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: sting to be reversed
+*/
+void rev_string(char *s)
+{
+	int i, len, len1;
+	char tmp;
+
+	len = 0;
+	len1 = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
+}

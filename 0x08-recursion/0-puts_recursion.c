d9883517a472a7a2ba62a11c7c85d123c5be0a31
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-13 15:18:22 +0300

    new

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _puts_recursion: print a string
+ * @s1: string
+ * Return: print the string.
+ */
+
+void _puts_recursion(char *s) {
+  printf("%s", s);
+}

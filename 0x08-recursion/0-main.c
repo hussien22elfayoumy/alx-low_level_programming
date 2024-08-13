4b7d9d3699e1caf304637212ed54f68124f68426
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-13 15:29:16 +0300

    nice

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -7,7 +7,8 @@
  * Return: Always 0.
  */
 
-int main (void) {
+int main(void)
+{
   _puts_recursion("Puts with recursion");
   return 0;
 }
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
  * _puts_recursion: print a string
  * @s1: string
- * Return: print the string.
+ * Return: void.
  */
 
-void _puts_recursion(char *s) {
-  printf("%s", s);
+void _puts_recursion(char *s)
+{
+  if (*s == '\0')
+  {
+    _putchar('\n');
+  }
+  _putchar(*s);
+  _puts_recursion(s + 1);
 }

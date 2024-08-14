611fcaf3db8ca0dd321d541441c4143914d448b5
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-14 16:13:07 +0300

    betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == '\0')
 	{
-		return haystack;
+		return (haystack);
 	}
 	while (*haystack != '\0')
 	{
@@ -30,7 +30,7 @@ char *_strstr(char *haystack, char *needle)
 
 			if (*n == '\0')
 			{
-				return haystack;
+				return (haystack);
 			}
 		}
 		haystack++;

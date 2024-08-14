abba2eed30905319a016c2b92cbe37edb061e7da
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-14 14:03:16 +0300

    nice work

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -6,9 +6,16 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
+	char *s = "hello";
+	char *f;
 
-  return 0;
+	f = _strchr(s, 'l');
+
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
 }

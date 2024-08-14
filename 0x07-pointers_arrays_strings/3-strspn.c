30c3a36803cc6ba5e499bc55e74a4964be79f4eb
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-14 15:11:57 +0300

    mmom

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strchr - a function that gets the length of a prefix substring.
+ * _strspn - a function that gets the length of a prefix substring.
  * @s: contains a string
  * @accept: contains a string
  * Return: Returns the number of bytes in the initial segment of s.
@@ -11,13 +11,19 @@ int _strspn(char *s, char *accept)
 {
 	int i = 0;
 	int count = 0;
+
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[i])
+			if (*s == accept[i])
 			{
 				count++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (count);
 			}
 		}
 		s++;
